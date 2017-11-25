71e6cf9bbb0c4c4085de59e4167c88d8e7e3b4c9
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-11-25 10:45:37 +0500

    Add files via upload

---
+++ MyQuizApp/Models/MyRoleProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MyQuizApp.Models
+{
+    public class MyRoleProvider:RoleProvider
+    {
+        MyQuizAppEntities db = new MyQuizAppEntities();
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            return db.UserTypes.Select(ut=> ut.Name).ToArray();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+
+            return db.UserTypes.Where(ut => ut.Users.FirstOrDefault().Name == username).Select(ut => ut.Name).ToArray();
+
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
