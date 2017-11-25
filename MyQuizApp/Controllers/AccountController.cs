56867004cae3eb27485802eaba2353bf93957b4f
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-11-25 10:44:55 +0500

    Add files via upload

---
+++ MyQuizApp/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MyQuizApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        MyQuizAppEntities db = new MyQuizAppEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string email, string password)
+        {
+            User user = new User();
+            user.Name = name;
+            user.Email = email;
+            user.Password = password;
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            if(!Roles.RoleExists("Student"))
+            {
+                Roles.CreateRole("Student");
+            }
+            
+
+            Roles.AddUserToRole(user.Email, "Student");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user == null) //agar email / password match nahin kartay tu 'user' null hota hay.
+            {
+                
+                ViewBag.Error = "Invalid email/password";
+                return View("Index");
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(user.Email, true);
+                //session banaya.
+                Session["email"] = user.Email;
+                Session["uid"] = user.Id;
+                Session["ut"] = user.UserType.Name;
+                Session["name"] = user.Name;
+
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ MyQuizApp/Controllers/QuizController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyQuizApp.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles="Administrator")]
+        public ActionResult Add(Quize quiz)
+        {
+            return View();
+        }
+    }
+}
